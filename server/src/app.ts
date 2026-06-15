fb52595ff2f9a15083656bdaf4ca004d9732d2df
John Francis Vecina <johnvecina640@gmail.com>
2026-06-15 12:27:31 +0800

    feat: implement authentication middleware to protect routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { errorMiddleware } from "./middleware/error.middleware";
 import authRoutes from './modules/auth/auth.routes'
+import { protect } from "./middleware/auth.middleware";
 
 const app = express();
 
@@ -18,4 +19,11 @@ app.get("/api/health", (_req, res) => {
 app.use('/api/auth', authRoutes)
 
 app.use(errorMiddleware);
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "You accessed a protected route",
+    user: (req as any).user,
+  });
+});
+
 export default app;
---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { env } from "../config/env";
+import { ApiError } from "../utils/apiError";
+import { User } from "../modules/user/user.model";
+
+export const protect = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    throw new ApiError(401, "Not authorized");
+  }
+
+  try {
+    const decoded = jwt.verify(token, env.jwt as string) as {
+      id: string;
+    };
+
+    const currentUser = await User.findById(decoded.id);
+
+    if (!currentUser) {
+      throw new ApiError(401, "User no longer exists");
+    }
+
+    (req as any).user = currentUser;
+
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid token");
+  }
+};
