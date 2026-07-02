ba58d26b9de44f0061c2c9addce08dafe5b2e095
John Francis Vecina <johnvecina640@gmail.com>
2026-07-02 18:43:34 +0800

    feat: implement createProject controller with service integration and error handling

---
+++ server/src/modules/project/project.controller.ts
@@ -0,0 +1,29 @@
+
+import { Request, Response } from 'express';
+import { createProjectService } from './project.service';
+import { ApiError } from '../../utils/apiError';
+
+export const createProject = async (req: Request, res: Response) => {
+    const project = await createProjectService(
+        req.body,
+        (req as any).user._id
+    );
+
+    if (!project) {
+        throw new ApiError(400, 'Project creation failed');
+    }
+
+    res.status(201).json({
+        status: "success",
+        data: {
+        id: project._id,
+        name: project.name,
+        description: project.description,
+        status: project.status,
+        deadline: project.deadline,
+        budget: project.budget,
+        client: project.client,
+        createdAt: project.createdAt,
+        },
+     });
+}
