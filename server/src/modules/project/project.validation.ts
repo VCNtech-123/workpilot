67de28bc27c7bdf749fcb0a69ee616593275e649
John Francis Vecina <johnvecina640@gmail.com>
2026-07-02 18:29:27 +0800

    feat: add validation for project creation with required fields and client ID check

---
+++ server/src/modules/project/project.validation.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { ApiError } from '../../utils/apiError';
+import mongoose from 'mongoose';
+
+export const validateCreateProject = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { name, client } = req.body || { };
+
+    if (!name || !client ) {    
+        throw new ApiError(400, 'Name and client are requred')
+    } 
+
+    if (!mongoose.Types.ObjectId.isValid(client)) {
+        throw new ApiError(400, 'InvalidClientId');
+    }
+
+    next();
+}
