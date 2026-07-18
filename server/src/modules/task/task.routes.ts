5f9bc676d6a6eec640463dd29b6855536e8bfd44
VCNtech-123 <johnvecina640@gmail.com>
2026-07-18 23:38:14 +0800

    feat: add task validation to POST route for task creation

--- server/src/modules/task/task.routes.ts
+++ server/src/modules/task/task.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware';
+import { validateCreateTask } from './task.validation';
 
 const router = Router();
 
-router.post("/", protect);
+router.post("/", protect, validateCreateTask );
 
 
 export default router;
