ca610118254a26ffda803bb36f09c185e83877aa
VCNtech-123 <johnvecina640@gmail.com>
2026-07-19 03:46:04 +0800

    feat: add getTaskById route to retrieve a task by ID

--- server/src/modules/task/task.routes.ts
+++ server/src/modules/task/task.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware';
 import { validateCreateTask } from './task.validation';
-import { createTask, getTasks } from './task.controller';
+import { createTask, getTasks, getTaskById } from './task.controller';
 
 const router = Router();
 
 router.post("/", protect, validateCreateTask, createTask );
 router.get("/", protect, getTasks);
-router.get("/:id", protect)
+router.get("/:id", protect, getTaskById)
 
 export default router;
