9dee331916e53ad418affb22277e2f74312fdb67
VCNtech-123 <johnvecina640@gmail.com>
2026-07-19 03:44:40 +0800

    feat: implement getTaskById endpoint for retrieving a task by ID with validation

--- server/src/modules/task/task.controller.ts
+++ server/src/modules/task/task.controller.ts
@@ -1,6 +1,8 @@
 
 import { Request, Response } from "express";
-import { createTaskService, getTaskService } from "./task.service";
+import { createTaskService, getTaskService, getTaskByIdService } from "./task.service";
+import mongoose from "mongoose";
+import { ApiError } from "../../utils/apiError";
 
 export const createTask = async (
     req: Request,
@@ -53,4 +55,39 @@ export const getTasks = async (
       createdAt: task.createdAt
     }))
   });
+}
+
+export const getTaskById = async (
+    req: Request,
+    res: Response
+) => {
+
+    const id = req.params.id as string;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new ApiError(400, "Invalid task ID");
+    }
+
+    const task = await getTaskByIdService(
+        id,
+        (req as any).user._id
+    );
+
+    if (!task) {
+        throw new ApiError(400, "Task not found");
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+        id: task._id,
+        title: task.title,
+        description: task.description,
+        status: task.status,
+        priority: task.priority,
+        dueDate: task.dueDate,
+        project: task.project,
+        createdAt: task.createdAt
+        }
+    })
 }
