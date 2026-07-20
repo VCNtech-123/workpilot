a2e215730adb1613ee51875eecbca669afc31bae
VCNtech-123 <johnvecina640@gmail.com>
2026-07-20 10:59:00 +0800

    feat: add restoreTask route to task routes

--- server/src/modules/task/task.routes.ts
+++ server/src/modules/task/task.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware';
 import { validateCreateTask } from './task.validation';
-import { createTask, getTasks, getTaskById, updateTaskById, deleteTask } from './task.controller';
+import { createTask, getTasks, getTaskById, updateTaskById, deleteTask, restoreTask } from './task.controller';
 
 const router = Router();
 
@@ -10,6 +10,6 @@ router.get("/", protect, getTasks);
 router.get("/:id", protect, getTaskById);
 router.put("/:id", protect, updateTaskById);
 router.delete("/:id", protect, deleteTask);
-router.patch(":id/restore", protect)
+router.patch(":id/restore", protect, restoreTask)
 
 export default router;
